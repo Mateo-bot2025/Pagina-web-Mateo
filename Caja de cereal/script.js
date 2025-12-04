d9518c35531c81bbadfbc2de9d47f615bb711998
Mateo-bot2025 <mateolaurencena5@gmail.com>
2025-12-04 16:46:28 -0300

    Add files via upload

---
+++ Caja de cereal/script.js
@@ -0,0 +1,4 @@
+function toggleInfo2() {
+    const section = document.getElementById("info-section-2");
+    section.style.display = section.style.display === "block" ? "none" : "block";
+}
---
+++ infografia/script.js
@@ -0,0 +1,4 @@
+function toggleInfo() {
+    const section = document.getElementById("info-section");
+    section.style.display = section.style.display === "block" ? "none" : "block";
+}
